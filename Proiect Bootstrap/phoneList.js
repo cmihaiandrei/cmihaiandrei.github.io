d96360e70151843de9740b74cf66accf7e055bbd
cmihaiandrei <43827857+cmihaiandrei@users.noreply.github.com>
2019-07-02 12:08:47 +0300

    Update phoneList.js

--- Proiect Bootstrap/phoneList.js
+++ Proiect Bootstrap/phoneList.js
@@ -2,15 +2,18 @@ mainPage.innerHTML += `
 <div id="welcome" class="alert alert-info" role="alert">
 	Bun venit pe GSM Shop!
 </div>
-		<div id="phone-list">
-		</div>
+<div id="phone-list"></div>
+
+
+
+</div>
+
 `;
 // 
 APP.renderPhoneList = (productArray) => {
 
 	const phoneList = document.querySelector('#phone-list');
 	phoneList.innerHTML = '';
-
 	productArray.forEach((elem) => {
 
 		phone = new Phone(elem.name, elem.price, elem.release, elem.added, elem.pic);

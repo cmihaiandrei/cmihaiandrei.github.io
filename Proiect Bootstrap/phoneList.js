42741fbff024e52b3c698f62af6a0ae66fda5ea6
cmihaiandrei <43827857+cmihaiandrei@users.noreply.github.com>
2019-07-02 12:45:18 +0300

    !

--- Proiect Bootstrap/phoneList.js
+++ Proiect Bootstrap/phoneList.js
@@ -2,10 +2,14 @@ mainPage.innerHTML += `
 <div id="welcome" class="alert alert-info" role="alert">
 	Bun venit pe GSM Shop!
 </div>
+<div class="media">
+  <img src="https://previews.123rf.com/images/houbacze/houbacze1701/houbacze170100253/69176246-red-easy-vector-illustration-isolated-paper-bubble-banner-important-this-element-is-well-adapted-for.jpg" class="mr-3" alt="...">
+  <div class="media-body">
+    <h5 class="mt-0">Important news!</h5>
+    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+  </div>
+</div>
 <div id="phone-list"></div>
-
-
-
 </div>
 
 `;
